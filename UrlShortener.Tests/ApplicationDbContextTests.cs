b1f17ab54634aa113a21a3c5bf1067bb7bdc592a
Chuma <114024209+chumavii@users.noreply.github.com>
2025-10-27 19:54:27 -0400

    Add ApplicationDbContext index configuration tests

--- UrlShortener.Tests/ApplicationDbContextTests.cs
+++ UrlShortener.Tests/ApplicationDbContextTests.cs
@@ -1,6 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using UrlShortener.Data;
+using UrlShortener.Models;
+using Xunit;
+
 namespace UrlShortener.Tests
 {
     public class ApplicationDbContextTests
     {
+        [Fact]
+        public void OnModelCreating_ShouldConfigureUniqueIndexForOriginalUrl()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            using var context = new ApplicationDbContext(options);
+
+            // Act
+            var entityType = context.Model.FindEntityType(typeof(UrlMapping));
+            var index = entityType?.GetIndexes()
+                .SingleOrDefault(i => i.Properties.Any(p => p.Name == nameof(UrlMapping.OriginalUrl)));
+
+            // Assert
+            Assert.NotNull(entityType);
+            Assert.NotNull(index);
+            Assert.True(index!.IsUnique);
+        }
+
+        [Fact]
+        public void OnModelCreating_ShouldConfigureUniqueIndexForShortCode()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            using var context = new ApplicationDbContext(options);
+
+            // Act
+            var entityType = context.Model.FindEntityType(typeof(UrlMapping));
+            var index = entityType?.GetIndexes()
+                .SingleOrDefault(i => i.Properties.Any(p => p.Name == nameof(UrlMapping.ShortCode)));
+
+            // Assert
+            Assert.NotNull(entityType);
+            Assert.NotNull(index);
+            Assert.True(index!.IsUnique);
+        }
     }
 }
