2ef5225507ae07dd8e0d9228b314a7201b4cfc7d
Chuma <chuxnwuba@gmail.com>
2025-11-02 00:48:52 -0400

    Disable test parallelization

---
+++ UrlShortener.Tests/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using Xunit;
+
+// Disables parallel test execution to prevent race conditions when multiple WebApplicationFactory instances run migrations simultaneously
+// Without this, multiple WebApplicationFactory instances may attempt to apply EF Core migrations concurrently.
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
